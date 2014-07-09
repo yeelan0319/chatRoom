cfe39f798090ddb844e874d659c927b73c88381b
Yiran Mao <yiranmao@gmail.com>
2014-07-09 14:39:46 -0400

    BUG FIX: express static server middleware should be used after others, because it will block and close link if matched file found.

--- public_html/node-server/expressRouting.js
+++ public_html/node-server/expressRouting.js
@@ -2,9 +2,9 @@ var express = require("express");
 var responseJson = require('./responseJson');
 
 module.exports = function(app){
-	app.express.use(express.static(__dirname + '/../public'));
     app.express.use(app.middleware.cookieParserFunction);
     app.express.use(app.middleware.sessionParserFunction);
+    app.express.use(express.static(__dirname + '/../public'));
 
     //routing
     app.express.get('/sessionExtension', function(req, res){
