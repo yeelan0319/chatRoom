cc03c8b53154347bbb4edb3ab1524a61783cc952
Yiran <yiran@yiran-server.(none)>
2014-07-18 16:19:41 -0400

    installer for the environment

---
+++ public_html/installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Start installing environment for chat room"
+echo ""
+
+killall -9 node
+/usr/bin/mongo admin --eval "db.shutdownServer()"
+apt-get update
+apt-get -y -q --purge remove nodejs
+apt-get -y -q --purge remove mongodb
+
+apt-get -y -q install mongodb-org
+mkdir -p /data/db
+
+apt-get -y -q install nodejs
+apt-get -y -q install npm
+cd node-server && npm install
+npm dedupe
+
+cd /path/to/ && openssl genrsa -des3 -out server.key 1024
+cd /path/to/ && openssl req -new -key server.key -out server.csr
+cd /path/to/ && openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+cd /path/to/ && cp server.key server.key.orig
+cd /path/to/ && openssl rsa -in server.key.orig -out server.key
